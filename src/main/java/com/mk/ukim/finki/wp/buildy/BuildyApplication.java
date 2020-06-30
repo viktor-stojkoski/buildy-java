c8244c38630bf9cd76ac7c1370b8d94b8590a00d
Viktor Stojkoski <viktor.98@hotmail.com>
2020-06-30 23:22:22 +0200

    spring boot project initialization

---
+++ src/main/java/com/mk/ukim/finki/wp/buildy/BuildyApplication.java
@@ -0,0 +1,13 @@
+package com.mk.ukim.finki.wp.buildy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mk/ukim/finki/wp/buildy/BuildyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk.ukim.finki.wp.buildy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
